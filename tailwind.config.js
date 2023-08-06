70990e5fccf3f585ea0305e94b32e919fb00c132
Ritika Kumar <kumarritika666@gmail.com>
2023-08-06 20:47:32 +1400

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      width: {
+        108: "34rem",
+        118: "44rem",
+        138: "64rem",
+        // Add more custom width values as needed
+      },
+    },
+  },
+  plugins: [],
+};
